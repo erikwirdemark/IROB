2172108d0205dbcddb876f59e0145fa36bba05ad
erikwirdemark <133378956+erikwirdemark@users.noreply.github.com>
2026-09-10 17:21:54 +0200

    working solution with whitespace removed

--- src/kinematics_assignment/kinematics_assignment/IK_functions.py
+++ src/kinematics_assignment/kinematics_assignment/IK_functions.py
@@ -1,17 +1,10 @@
-
 #! /usr/bin/env python3
 
-
-
 """
-
     # Erik Wirdemark
-
     # erikwir@kth.se
 
 """
-
-
 
 import math
 import numpy as np
@@ -19,14 +12,13 @@ from rclpy.logging import get_logger
 
 logger = get_logger("kuka_IK")
 
+
 def scara_IK(point):
     x = point[0]
     y = point[1]
     z = point[2]
+
     q = [0.0, 0.0, 0.0]
-
-
-
     """
     Fill in your IK solution here and return the three joint values in q
     """
@@ -34,7 +26,7 @@ def scara_IK(point):
     l0 = 0.07
     l1 = 0.3
     l2 = 0.35
-
+    
     r = (x-l0)**2 + y**2
     cosine_q2 = (r - l1**2 - l2**2) / (2 * l1 * l2)
 
@@ -45,7 +37,6 @@ def scara_IK(point):
     q1 = math.atan2(y, x-l0) - math.atan2(l2 * math.sin(q2), l1 + l2 * math.cos(q2))
     q3 = z
     q = [q1, q2, q3]
-
     return q
 
 def kuka_IK(point, R, joint_positions):
@@ -56,25 +47,26 @@ def kuka_IK(point, R, joint_positions):
 
     """
     Fill in your IK solution here and return the seven joint values in q
+
     """
 
     X = [x, y, z]
     Epsilon_x = np.array([float('inf')])
-
-    threshold = 0.001
+    max_iters = 60
+    threshold = 0.005
 
     iters = 0
     while np.linalg.norm(Epsilon_x) > threshold:
         iters += 1
         transformations = forward_kinematics(q)
+
         j = jacobian(q, transformations)
         X_hat = transformations[-1] # base layer to end effector
 
         position_curr = X_hat[0:3, 3]
         rotation_curr = X_hat[0:3, 0:3]
 
-        Epsilon_pos = point - position_curr
-
+        Epsilon_pos = position_curr - point
 
         R_err = R @ rotation_curr.T
         Epsilon_rotation = 0.5 *  np.array([R_err[2, 1] - R_err[1, 2],
@@ -84,17 +76,16 @@ def kuka_IK(point, R, joint_positions):
         Epsilon_x = np.concatenate([Epsilon_pos, Epsilon_rotation])
         # logger.info(f'This is the size of the jacobian: {j.size}')
         # logger.info(f'This is the size of Epsilon_x: {Epsilon_x}')
-
         Epsilon_q = np.linalg.pinv(j) @ Epsilon_x
-        q = q + 0.5 * Epsilon_q
+        q = q - Epsilon_q
 
     return list(q)
+
 
 def forward_kinematics(q):
     L = 0.4
     M = 0.39
     q1, q2, q3, q4, q5, q6, q7 = q
-
     DH_params = []
     DH_params.append([np.pi/2, 0.311, 0, q1])
     DH_params.append([-np.pi/2, 0, 0, q2])
@@ -113,10 +104,10 @@ def forward_kinematics(q):
 
     return transformations
 
+
 def jacobian(q, transformations=None):
     if transformations is None:
         raise ValueError("You forgot to pass the trnasformations")
-
     p_e = transformations[-1][0:3, 3]
     J = np.zeros((6, 7))
 
@@ -127,11 +118,9 @@ def jacobian(q, transformations=None):
         w = z_i # anfular velocity
         J[0:3, i] = v
         J[3:6, i] = w
-
     # logger.info(f'size of jacobian: {J.size}')
+
     return J
-
-
 
 def DH_transformation(DH_params_row):
     alpha, d, a, theta = DH_params_row
@@ -140,4 +129,5 @@ def DH_transformation(DH_params_row):
                   [np.sin(theta), np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha), a*np.sin(theta)],
                   [0, np.sin(alpha), np.cos(alpha), d],
                   [0, 0, 0, 1]])
+
     return T
